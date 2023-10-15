29ea47f118c8d54cf0d094f499d281eaad2ebee6
Ayushman Singh <amansinghayush@gmail.com>
2023-10-15 20:47:26 +0530

    list flag added

--- main.go
+++ main.go
@@ -13,15 +13,21 @@ func main(){
 	
 	var delInd , completeIndex *int
 	var addVal string
+	var list *bool
+
 	flag.StringVar(&addVal,"add","", "Use this flag followed by name of the task under quotation marks")
 	
 	delInd= flag.Int("delete",0 ,"Give the index of the task to be deleted")
 	
 	completeIndex= flag.Int("completed",0 ,"Give the index of the task to be marked completed")
 
+	list= flag.Bool("ls",false,"Use this flag for Looking at all the tasks")
+
 	flag.Parse();
 
 	fmt.Printf("%s\n",addVal)
 
 	fmt.Printf("%d\n%d\n",*delInd,*completeIndex)
+
+	fmt.Printf("%v\n",*list)
 }
