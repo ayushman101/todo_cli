f01c4c66099480b9293b4c3708f8731da4d8f32b
Ayushman Singh <amansinghayush@gmail.com>
2023-10-15 20:42:18 +0530

    more flags added

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"flag"
 )
 
+//TODO: more flags to Add:  delete, completed, list
+
 func main(){
 	
+	var delInd , completeIndex *int
 	var addVal string
-	flag.StringVar(&addVal,"add","Sample Task", "Use this flag followed by name of the task under quotation marks")
+	flag.StringVar(&addVal,"add","", "Use this flag followed by name of the task under quotation marks")
+	
+	delInd= flag.Int("delete",0 ,"Give the index of the task to be deleted")
+	
+	completeIndex= flag.Int("completed",0 ,"Give the index of the task to be marked completed")
 
 	flag.Parse();
 
 	fmt.Printf("%s\n",addVal)
+
+	fmt.Printf("%d\n%d\n",*delInd,*completeIndex)
 }
